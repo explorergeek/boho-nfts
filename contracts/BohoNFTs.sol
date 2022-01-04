ac4cac9e3f8f13b6640912231c86b6f76371db3e
Courtney Jensen <courtney@prologmobile.com>
2022-01-03 17:59:28 -0800

    removed extra comments.

--- contracts/BohoNFTs.sol
+++ contracts/BohoNFTs.sol
@@ -6,45 +6,31 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
-// We need to import the helper functions from the contract that we copy/pasted.
 import { Base64 } from "./libraries/Base64.sol";
 
-// We inherit the contract we imported. This means we'll have access
-// to the inherited contract's methods.
 contract BohoNFT is ERC721URIStorage {
-  // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
   
-// This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
-  // So, we make a baseSvg variable here that all our NFTs can use.
   string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
   string svgPartTwo = "' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
 
-  // I create three arrays, each with their own theme of random words.
-  // Pick some random funny words, names of anime characters, foods you like, whatever! 
   string[] firstWords = ["Pink", "Orange", "Yellow", "Scarlet", "Ivy", "Aqua"];
   string[] secondWords = ["Positive", "Majestic", "Empowered", "Heroic", "Fierce", "Tranquil"];
   string[] thirdWords = ["Llama", "Sun", "Moon", "Cactus", "Tree", "Rainbow"];
-
- // Declare a bunch of colors.
   string[] colors = ["#f143f7", "#fcff33", "#00ffd0", "#07f702", "#ff2448", "#ff771c"];
 
 
   //events
   event NewEpicNFTMinted(address sender, uint256 tokenId);
-
-  // We need to pass the name of our NFTs token and its symbol.
+  
   constructor() ERC721 ("SquareNFT", "SQUARE") {
     console.log("This is my BohoNFT contract!");
   }
   
-  // I create a function to randomly pick a word from each array.
   function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
-    // I seed the random generator. More on this in the lesson. 
     uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
-    // Squash the # between 0 and the length of the array to avoid going out of bounds.
     rand = rand % firstWords.length;
     return firstWords[rand];
   }
@@ -61,7 +47,6 @@ contract BohoNFT is ERC721URIStorage {
     return thirdWords[rand];
   }
 
- // Pick a random color.
   function pickRandomColor(uint256 tokenId) public view returns (string memory) {
     uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
     rand = rand % colors.length;
@@ -73,21 +58,16 @@ contract BohoNFT is ERC721URIStorage {
       return uint256(keccak256(abi.encodePacked(input)));
   }
 
-  // A function our user will hit to get their NFT.
   function makeABohoNFT() public {
-     // Get the current tokenId, this starts at 0.
     uint256 newItemId = _tokenIds.current();
 
-    // We go and randomly grab one word from each of the three arrays.
     string memory first = pickRandomFirstWord(newItemId);
     string memory second = pickRandomSecondWord(newItemId);
     string memory third = pickRandomThirdWord(newItemId);    
     string memory combinedWord = string(abi.encodePacked(first, second, third));
 
-    // Add the random color in.
     string memory randomColor = pickRandomColor(newItemId);
-
-    // I concatenate it all together, and then close the <text> and <svg> tags.
+ 
     string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
     
@@ -97,10 +77,10 @@ contract BohoNFT is ERC721URIStorage {
             string(
                 abi.encodePacked(
                     '{"name": "',
-                    // We set the title of our NFT as the generated word.
+                    // Set the title of the NFT as the generated word.
                     combinedWord,
                     '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-                    // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                    // Add data:image/svg+xml;base64 and then append the base64 encode the svg.
                     Base64.encode(bytes(finalSvg)),
                     '"}'
                 )
@@ -108,26 +88,23 @@ contract BohoNFT is ERC721URIStorage {
         )
     );
 
-    // Just like before, we prepend data:application/json;base64, to our data.
+    // Prepend data:application/json;base64, to our data.
     string memory finalTokenUri = string(
         abi.encodePacked("data:application/json;base64,", json)
     );
 
-    console.log("\n--------------------");
+    console.log("\n-----");
     console.log(finalTokenUri);
-    console.log("--------------------\n");
+    console.log("-----\n");
 
-     // Actually mint the NFT to the sender using msg.sender.
     _safeMint(msg.sender, newItemId);
 
     // Set the NFTs data. The second variable is for the website where we can get our json data (the metadata for the nft)
     //UPDATE THIS WHEN UPDATING JSON DATA
     _setTokenURI(newItemId, finalTokenUri);
     
-    // Increment the counter for when the next NFT is minted.
     _tokenIds.increment();
 
-    //This will show us when the NFT is minted and to who
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
     // Implements event
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,18 +3,11 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
-    
-    // Call the function.
+
     let txn = await nftContract.makeABohoNFT()
     // Wait for it to be mined.
     await txn.wait()
-    console.log("Minted NFT #1")
-
-    // // Mint another NFT for fun.
-    // txn = await nftContract.makeABohoNFT()
-    // // Wait for it to be mined.
-    // await txn.wait()
-    // console.log("Minted NFT #2")
+    console.log("Minted NFT!")
   };
   
   const runMain = async () => {
